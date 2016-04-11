9afe51acdd3b58e58d437a709c9d2062bd090949
wjdgo813 <wjdgo813@naver.com>
2016-04-11 22:14:11 +0900

    네비게이션 카테고리 상세보기 수정

--- app/src/main/java/com/example/lg/markertree/holder/CategoryItemViewHolder.java
+++ app/src/main/java/com/example/lg/markertree/holder/CategoryItemViewHolder.java
@@ -1,5 +1,6 @@
 package com.example.lg.markertree.holder;
 
+import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.lg.markertree.R;
 import com.example.lg.markertree.result.category_list.CategoryItem;
 
@@ -18,10 +21,11 @@ public class CategoryItemViewHolder extends RecyclerView.ViewHolder {
     ImageView thumbnailImage;
     TextView txtBook_name,txtBook_url,textcnt,txtFavorite;
     public CardView hotBookMark;
+    Context mContext;
 
-
-    public CategoryItemViewHolder(View itemView) {
+    public CategoryItemViewHolder(View itemView,Context mContext) {
         super(itemView);
+        this.mContext = mContext;
         hotBookMark = (CardView)itemView.findViewById(R.id.hotBookMark);
         txtBook_name = (TextView)itemView.findViewById(R.id.textName);
         txtBook_url = (TextView)itemView.findViewById(R.id.textUrl);
@@ -40,5 +44,12 @@ public class CategoryItemViewHolder extends RecyclerView.ViewHolder {
         txtBook_name.setText(book_name);
         txtBook_url.setText(item.book_url);
         textcnt.setText(item.cnt);
+        String imageUrl="http://52.193.206.21:3000/"+item.book_thumb;
+        Glide.with(mContext)
+                .load(imageUrl)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .into(thumbnailImage);
     }
 }
--- app/src/main/java/com/example/lg/markertree/ui/CategoryList/CategoryItemAdapter.java
+++ app/src/main/java/com/example/lg/markertree/ui/CategoryList/CategoryItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.lg.markertree.ui.CategoryList;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import com.example.lg.markertree.R;
 import com.example.lg.markertree.holder.CategoryItemViewHolder;
 import com.example.lg.markertree.result.category_list.CategoryItem;
+import com.example.lg.markertree.ui.read.ReadActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,14 +49,27 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemViewHo
     public CategoryItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.view_hot_item, parent, false);
-        CategoryItemViewHolder holder = new CategoryItemViewHolder(view);
+        CategoryItemViewHolder holder = new CategoryItemViewHolder(view,mContext);
 
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(CategoryItemViewHolder holder, int position) {
+    public void onBindViewHolder(CategoryItemViewHolder holder, final int position) {
+
         holder.setData(items.get(position));
+        holder.hotBookMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, ReadActivity.class);
+
+                intent.putExtra("bookPos", items.get(position).book_idx);
+                intent.putExtra("imgUrl","http://52.193.206.21:3000/"+items.get(position).book_thumb); //후에 미리보기 처리
+                intent.putExtra("bookName", items.get(position).book_name);
+                intent.putExtra("bookUrl", items.get(position).book_url);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
